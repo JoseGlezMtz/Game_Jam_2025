581ea1bc193187d6cafbf64c24c7755e50534a43
luisda25 <luisdaf04@outlook.com>
2025-05-15 22:00:12 -0600

    feat: added bullet scene

--- game_project/hitbox.gd
+++ game_project/hitbox.gd
@@ -1,1 +1,19 @@
-extends CollisionShape2D
+extends Sprite2D
+
+var hp: int = 5
+
+signal hp_changed
+signal died
+
+func take_damage( dmg ):
+	set_hp(hp - dmg) 
+
+func set_hp( new_hp ):
+	hp = new_hp
+	if hp <= 0:
+		die()
+		
+func die():
+	emit_signal("died")
+	queue_free()
+	
